b07f9cf87e2bae85946c05a55f5285899fae8b5c
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-05-21 18:53:57 +0700

    Add game obj

--- arcore_facemakeup/Assets/Scripts/CameraSample.cs
+++ arcore_facemakeup/Assets/Scripts/CameraSample.cs
@@ -7,6 +7,7 @@ using System;
 public class CameraSample : MonoBehaviour
 {
 	Button Capture;
+
 	void Update()
 	{
 		Capture.GetComponent<Button>().onClick.AddListener(delegate { captureClicked(); });
