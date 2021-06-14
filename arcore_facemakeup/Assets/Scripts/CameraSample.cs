f5f8bae81d923194be266e07b7d258216625a358
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-06-14 07:46:23 +0700

    Update take picture function

--- arcore_facemakeup/Assets/Scripts/CameraSample.cs
+++ arcore_facemakeup/Assets/Scripts/CameraSample.cs
@@ -7,62 +7,49 @@ using System;
 public class CameraSample : MonoBehaviour
 {
 	Button Capture;
+	bool takePicture;
 
 	void Update()
 	{
-		Capture.GetComponent<Button>().onClick.AddListener(delegate { captureClicked(); });
+		Capture.GetComponent<Button>().onClick.AddListener(delegate { CaptureClicked(); });
 	}
 
-	public void captureClicked()
+   // private void OnRenderImage(RenderTexture source, RenderTexture destination)
+   // {
+   //     if(takePicture)
+   //     {
+			//takePicture = false;
+
+			//var tempRend = RenderTexture.GetTemporary(source.width, source.height);
+			//Graphics.Blit(source, tempRend);
+
+			//Texture2D tempText = new Texture2D(source.width, source.height, TextureFormat.RGBA32, false);
+			//Rect rect = new Rect(0, 0, source.width, source.height);
+			//tempText.ReadPixels(rect, 0, 0, false);
+			//tempText.Apply();
+   //     }
+   // }
+
+    public void CaptureClicked()
     {
-        if (NativeCamera.IsCameraBusy())
-				return;
+		StartCoroutine(TakeScreenshotAndSave());
+	}
 
-			if (Input.mousePosition.x < Screen.width / 2)
-			{
-				// Take a picture with the camera
-				// If the captured image's width and/or height is greater than 512px, down-scale it
-				TakePicture(512);
-			}
-    }
-
-	private void TakePicture(int maxSize)
+	private IEnumerator TakeScreenshotAndSave()
 	{
-		NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
-		{
-			Debug.Log("Image path: " + path);
-			if (path != null)
-			{
-				// Create a Texture2D from the captured image
-				Texture2D texture = NativeCamera.LoadImageAtPath(path, maxSize);
-				if (texture == null)
-				{
-					Debug.Log("Couldn't load texture from " + path);
-					return;
-				}
+		yield return new WaitForEndOfFrame();
 
-				// Assign texture to a temporary quad and destroy it after 5 seconds
-				GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-				quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
-				quad.transform.forward = Camera.main.transform.forward;
-				quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		ss.Apply();
 
-				Material material = quad.GetComponent<Renderer>().material;
-				if (!material.shader.isSupported) // happens when Standard shader is not included in the build
-					material.shader = Shader.Find("Legacy Shaders/Diffuse");
-
-				material.mainTexture = texture;
-				NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(texture, Application.productName, DateTime.Now.ToFileTime() + ".jpg", (success, path) => Debug.Log("Media save result: " + success + " " + path));
-				Debug.Log("Permission result: " + permission);
-				Destroy(quad, 5f);
-
-				// If a procedural texture is not destroyed manually, 
-				// it will only be freed after a scene change
-				Destroy(texture, 5f);
-			}
-		}, maxSize);
+		// Save the screenshot to Gallery/Photos
+		NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(ss, "GalleryTest", "Image.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
 
 		Debug.Log("Permission result: " + permission);
+
+		// To avoid memory leaks
+		Destroy(ss);
 	}
 
 }
